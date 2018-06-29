855c2770467b95acf005f5cba227a763a63a2fa9
woonaa <lucky.woon.j@gmail.com>
2018-06-29 10:47:56 +0900

    생활코딩 node.Js 시작
    기본문법 및 주요기능

--- nodeJs/main.js
+++ nodeJs/main.js
@@ -1,15 +1,19 @@
-var http = require('http');
-var fs = require('fs');
-var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-        url = '/index.html';
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const app = http.createServer(function(request,response){
+    const _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    console.log(queryData);
+    if(_url === '/'){
+        _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url === '/favicon.ico'){
         return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(fs.readFileSync(__dirname + _url));
 
 });
 app.listen(3000);
