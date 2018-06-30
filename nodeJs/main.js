8231a8c6cd7f0c5c5036dcb2aa8557aa0d7fb001
woonaa <lucky.woon.j@gmail.com>
2018-06-30 20:15:09 +0900

    Web2. node.js 생활코딩 수업 진행 중
    CRUD

--- nodeJs/main.js
+++ nodeJs/main.js
@@ -2,18 +2,69 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-const app = http.createServer(function(request,response){
-    const _url = request.url;
-    const queryData = url.parse(_url, true).query;
-    console.log(queryData);
-    if(_url === '/'){
-        _url = '/index.html';
-    }
-    if(_url === '/favicon.ico'){
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + _url));
+const app = http.createServer(function(request, response) {
+  const _url = request.url;
+  const queryData = url.parse(_url, true).query;
+  const pathname = url.parse(_url, true).pathname;
+  console.log(queryData)
 
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+        const title = "welcome";
+        const description1 = "hello nodejs"
+        const template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=html">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description1}</p>
+          </body>
+          </html>
+          `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+        const title = queryData.id;
+        const template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=html">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end('not found');
+  }
 });
+
 app.listen(3000);
