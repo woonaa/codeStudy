44da46673b7c49489790ca7a75837595eabc90f6
woonaa <lucky.woon.j@gmail.com>
2018-07-03 23:56:03 +0900

    생활코딩 수업 app제작 전 완료

--- nodeJs/main.js
+++ nodeJs/main.js
@@ -2,6 +2,33 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+function templateHMTL(title, list, body) {
+  return `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${body}
+      </body>
+      </html>
+      `;
+}
+
+function templateList(filelist) {
+  let list = '<ul>';
+  let i = 0;
+  while (i < filelist.length) {
+    list = list + `<li> <a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + '</ul>';
+  return list;
+}
 const app = http.createServer(function(request, response) {
   const _url = request.url;
   const queryData = url.parse(_url, true).query;
@@ -10,55 +37,46 @@ const app = http.createServer(function(request, response) {
 
   if (pathname === '/') {
     if (queryData.id === undefined) {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+
+      //파일리스트 가져오기
+      fs.readdir('./data', function(error, filelist) {
         const title = "welcome";
         const description1 = "hello nodejs"
-        const template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=html">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description1}</p>
-          </body>
-          </html>
-          `;
+
+        const list = templateList(filelist);
+
+
+        const template = templateHMTL(title, list, `<h2>${title}</h2><p>${description1}</p>`);
         response.writeHead(200);
         response.end(template);
-      });
+      })
+
+
     } else {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-        const title = queryData.id;
-        const template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=html">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-        response.writeHead(200);
-        response.end(template);
+      //파일리스트 가져오기
+      fs.readdir('./data', function(error, filelist) {
+        fs.readFile(`./data/${queryData.id}`, 'utf8', function(err, description) {
+          const title = queryData.id;
+          const list = templateList(filelist);
+          const template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+          //왜 templateHMTL(title, list, `<h2>${title}</h2><p>${description1}</p>`); 이렇게 하면 실행이 안되는가?
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
